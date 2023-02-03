cc2956495bc60fac0b2aa24c15a14f72c6db7c95
Jacek Papiniak <jacekpapiniak@gmail.com>
2023-02-03 21:03:30 +0000

    Mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log('GULP TEST');
+const menuMobile = document.querySelector('#menu-mobile');
+const overlay = document.querySelector('#overlay');
+
+const show = (element) =>{
+    return function (){
+        element.classList.add('visible');
+        element.classList.remove('hidden');
+    }
+}
+
+const hide = (element) =>{
+    return function (){
+        element.classList.add('hidden');
+        element.classList.remove('visible');
+    }
+}
+
+document.querySelector('#toggle-mobile').addEventListener('click', show(overlay));
+document.querySelector('#toggle-mobile').addEventListener('click', show(menuMobile));
+
+document.querySelector('#hide-menu').addEventListener('click', hide(overlay));
+document.querySelector('#hide-menu').addEventListener('click', hide(menuMobile));
